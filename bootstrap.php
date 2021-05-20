f0304a8f1e9148a31bdd5e1e0fa28eb91ebf32a2
Steve McDougall <juststevemcd@gmail.com>
2021-05-20 10:58:36 +0100

    Testing adding highlight-php for syntax highlighting

--- bootstrap.php
+++ bootstrap.php
@@ -1,10 +1,12 @@
 <?php
 
-use App\Listeners\GenerateFeed;
+use App\ParsedownParser;
 use TightenCo\Jigsaw\Jigsaw;
+use App\Listeners\GenerateFeed;
 use App\Listeners\GenerateIndex;
 use App\Listeners\GenerateSitemap;
 use App\Listeners\GenerateReadingTime;
+use Mni\FrontYAML\Markdown\MarkdownParser;
 
 // Generate reading time per article
 $events->afterCollections(GenerateReadingTime::class);
@@ -17,3 +19,5 @@ $events->afterBuild(GenerateSitemap::class);
 
 // Generate RSS Feed
 $events->afterBuild(GenerateFeed::class);
+
+$container->bind(MarkdownParser::class, ParsedownParser::class);
