abc5854ca06e8d57ca6d30e41b26f97ff4a54813
Steve McDougall <juststevemcd@gmail.com>
2021-05-26 10:14:08 +0100

    Update refresh

--- source/_components/packages/card.blade.php
+++ source/_components/packages/card.blade.php
@@ -4,14 +4,15 @@
             <div class="space-y-6">
                 <h2 class="text-2xl leading-8 font-bold tracking-tight">
                     <a
-                        class="cursor-pointer hover:underline text-gray-600 dark:text-gray-200 hover:text-gray-800 dark:hover:text-white text-xl md:text-2xl"
+                        class="cursor-pointer hover:underline text-gray-600 dark:text-gray-200 hover:text-gray-800 dark:hover:text-white text-xl md:text-2xl flex items-center space-x-2 group-hover"
                         aria-label="Check out &quot;{{ $package->title }}&quot;"
                         title="Check out &quot;{{ $package->title }}&quot; on packagist"
                         href="https://packagist.org/packages/{{ $package->title }}"
                         target="__blank"
                         rel="noopener nofollow"
                     >
-                        {{ $package->title }}
+                        <svg class="w-5 h-5 text-gray-400 dark:text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"></path></svg>
+                        <span>{{ ucwords(str_replace('-', ' ', substr($package->title, 14))) }}</span>
                     </a>
                 </h2>
                 <div class="max-w-3xl leading-7 text-md space-y-3 text-gray-800 dark:text-gray-50">
@@ -36,8 +37,9 @@
         </div>
         <dl class="text-right">
             <dt class="sr-only">Published on</dt>
-            <dd class="text-base leading-6 font-medium">
-                <time datetime="{{ $package->getPublished()->toDateString() }}">
+            <dd class="flex flex-col text-base leading-6 font-medium space-y-2">
+                <span class="text-sm">Last updated on:</span>
+                <time class="text-md" datetime="{{ $package->getPublished()->toDateString() }}">
                     {{ $package->getPublished()->format('jS F Y') }}
                 </time>
             </dd>
