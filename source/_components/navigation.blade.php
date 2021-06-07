b93056c072354ecc00837b939929f11901a0ba42
Steve McDougall <juststevemcd@gmail.com>
2021-06-07 12:53:26 +0100

    Fixing stuffs

--- source/_components/navigation.blade.php
+++ source/_components/navigation.blade.php
@@ -1,5 +1,5 @@
-<header class="max-w-8xl py-12 px-6 md:px-24 lg:px-32">
-    <div class="w-full flex items-center justify-start space-x-6 md:space-x-12">
+<header class="max-w-8xl py-6 md:py-12 px-6 md:px-24 lg:px-32">
+    <div class="w-full md:flex md:items-center md:justify-start space-x-0 md:space-x-6 md:space-x-12">
         <a href="/" title="Return to the home page">
             <x-avatar class="h-14 w-14 md:h-20 md:w-20 lg:h-24 lg:w-24 cursor-pointer" />
         </a>
@@ -9,7 +9,7 @@
                     Steve McDougall
                 </h1>
             </a>
-            <nav>
+            <nav class="overflow-x-scroll">
                 <ul class="flex items-center justify-start space-x-3 md:space-x-6 uppercase">
                     @foreach ($page->navigation as $item)
                         <li>
