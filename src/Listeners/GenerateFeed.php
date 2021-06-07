9222200c46e7f35c52c377aafb58b751ad51849d
Steve McDougall <juststevemcd@gmail.com>
2021-06-07 14:09:36 +0100

    Extending RSS

--- src/Listeners/GenerateFeed.php
+++ src/Listeners/GenerateFeed.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Listeners;
 
+use Carbon\Carbon;
 use RuntimeException;
 use Suin\RSSWriter\Feed;
 use Suin\RSSWriter\Item;
@@ -47,6 +48,35 @@ class GenerateFeed
                     ->author($article->author)
                     ->pubDate((new \DateTime('@'.$article->date))->getTimestamp())
                     ->guid($article->getUrl(), true)
+                    ->preferCdata(true) // By this, title and description become CDATA wrapped HTML.
+                    ->appendTo($channel);
+            });
+
+            $jigsaw->getCollection('packages')->each(function ($package) use ($channel, $config) {
+                // Blog item
+                $item = new Item();
+                $item
+                    ->title($package->title)
+                    ->description($package->description)
+                    ->url($package->getUrl())
+                    ->author('JustSteveKing')
+                    ->pubDate(Carbon::parse($package->time)->timestamp)
+                    ->guid($package->getUrl(), true)
+                    ->preferCdata(true) // By this, title and description become CDATA wrapped HTML.
+                    ->appendTo($channel);
+            });
+
+            $jigsaw->getCollection('streams')->each(function ($stream) use ($channel, $config) {
+                // Blog item
+                $item = new Item();
+                $item
+                    ->title($stream->title)
+                    ->description($stream->description)
+                    ->contentEncoded($stream->getContent())
+                    ->url($stream->getUrl())
+                    ->author($stream->author)
+                    ->pubDate((new \DateTime('@'.$stream->date))->getTimestamp())
+                    ->guid($stream->getUrl(), true)
                     ->preferCdata(true) // By this, title and description become CDATA wrapped HTML.
                     ->appendTo($channel);
             });
