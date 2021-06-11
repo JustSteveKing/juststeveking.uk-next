5e7b6db66f755d47b7538b90dce7eaedc84bed75
Steve McDougall <juststevemcd@gmail.com>
2021-06-11 10:51:41 +0100

    Fixing manifest

---
+++ source/offline.blade.php
@@ -0,0 +1,39 @@
+---
+title: JustSteveKing - Consultant CTO, Software Engineer, Community Advocate | juststeveking.uk
+---
+
+@section('body')
+    <x-container>
+
+        <article class="max-w-3xl leading-7 text-md space-y-3 text-gray-800 dark:text-gray-50 mb-12">
+            <p>👋 Hey there, I'm Steve, but you may know me better as
+                <a href="https://twitter.com/JustSteveKing" target="_blank"
+                    onclick="window.fathom.trackGoal('CZEGD7I0', 0);" rel="noopener nofollow"
+                    title="Check out my profile on twitter &quot;@JustSteveKing&quot;"
+                    class="text-md font-semibold text-transparent bg-clip-text bg-gradient-to-tr from-indigo-500 to-pink-600">
+                    @JustSteveKing
+                </a>. I currently live in the Welsh Valleys, about 40 minutes outside of Cardiff.
+            </p>
+            <p>
+                I currently work as a Technical Lead at
+                <a href="https://sqft.capital" target="_blank" rel="noopener nofollow"
+                    title="Check out the company where I work, we do some cool stuff with data &quot;sqft.capital&quot;"
+                    class="text-md font-semibold text-transparent bg-clip-text bg-gradient-to-tr from-indigo-500 to-pink-600">
+                    sqft.capital
+                </a> a property development finance company based in the UK, where I spend most of my time
+                working on improving our platform aimed at property developers, and working on new features to find ways
+                we can add extra value to our datasets.
+            </p>
+            <p>
+                I also spend some time moonlighting as a consultant CTO, and freelance software engineer for various clients
+                from around the world.
+                As a consultant CTO I have been involved in some major Digital Transformation projects, API consulting, open
+                source consulting and advising companies on strategy.
+            </p>
+            <p>
+                When I am not writing code, or advising someone, I spend a lot of my spare time with my family, cooking, and
+                contributing to open source and the community.
+            </p>
+        </article>
+    </x-container>
+@endsection
